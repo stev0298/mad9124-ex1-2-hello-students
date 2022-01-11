1f7a73720b6a7c26a9e4a025f1d4f5a116b3cb5f
stev0298 <stev0298@algonquinlive.com>
2022-01-10 22:57:27 -0500

    completed step 2
    iterate over the "students" array and print "Hello <firstName> <lastName>" to the console.

--- helloStudents.js
+++ helloStudents.js
@@ -5,6 +5,9 @@ const students = require("./students.json");
 
 // 2. Iterate over the students array and print Hello with their full names to the console
 // e.g. Hello Walter Baker
+students.forEach(student => {
+    console.log(`Hello ${student.firstName} ${student.lastName}.`);
+})
 
 // 3. Print out the number of last names starting with the letter D
 // e.g. Count of last names starting with D is 1
