58b033a9bd44bb10bba34c5d7483e8b9ba46a3e5
stev0298 <stev0298@algonquinlive.com>
2022-01-10 23:35:08 -0500

    completed step 3
    print out the number of last names starting with the letter "D". note: i coded it so that you can pass any letter into the function, in case you want to return the count of last names starting with "B" instead.

--- helloStudents.js
+++ helloStudents.js
@@ -11,3 +11,23 @@ students.forEach(student => {
 
 // 3. Print out the number of last names starting with the letter D
 // e.g. Count of last names starting with D is 1
+function test (arr, letter) {
+    // Filter the original array to only include students whose last name starts with "letter":
+    let filteredArr = arr.filter(student => student.lastName.toLowerCase().startsWith(letter.toLowerCase()));
+
+    // Count how many students have a last name beginning with "letter":
+    const count = function (arr) {
+        let count = 0;
+        arr.forEach(student => {
+            count += 1;
+        });
+
+        return count;
+    };
+
+    // Output:
+    console.log(`${count(filteredArr)} student(s) have a last name starting with ${letter.toUpperCase()}.`);
+}
+
+// Call function:
+test(students, "D"); // You can filter and count by any letter.
